1d2afd89a0a1db5e491d7b2dc38b3bb0f024a924
threalwinky <voquangvu09112006@gmail.com>
2025-06-10 13:28:46 +0800

    upload files

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import *
 
 app = Flask(__name__)
+
+log = []
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/ping')
+def ping():
+    log.append('ping')
+    return 'pong'
+
+@app.route('/log')
+def get_log():
+    return log
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
